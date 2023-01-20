ad3004544988881c7d2cb0a1075fc88026fcbf5f
Raul Gambalonga <gambalongadev@gmail.com>
2023-01-20 12:08:55 +0100

    add updateBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
+
 const TURNS = {
   X:'x',
   O:'o'
 }
 
-const board = Array(9).fill(null)
+
+const Square = ( {children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+      updateBoard()
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
+const [board, setBoard] = useState(
+  Array(9).fill(null)
+  );
+const [turn, setTurn] = useState(TURNS.X)
+
+const updateBoard = () => {
+
+}
+
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
@@ -13,18 +38,27 @@ function App() {
       {
         board.map((_, index) => {
           return (
-            <div className="cell" key={index}>
-            <span className="cell_content">
-              {index}
-            </span> 
-
-            </div>
+            <Square
+            key={index}
+            index={index}
+            updateBoard={updateBoard}
+            >
+            {index}
+            </Square>
           )
         })
       }
-
       </section>
 
+      <section className="turn">
+      <Square isSelected={turn === TURNS.X}>
+       {TURNS.X} 
+       </Square>
+      <Square isSelected={turn === TURNS.O}> 
+      {TURNS.O} 
+      </Square>
+
+      </section>
     </main>
   )
 }
